e87f2f5ccee31d8c4c3bf5cae09e63ff5f1ba23c
Guruh Edi Purwanto <guruhedipurwanto@gmail.com>
2019-12-31 15:07:44 +0800

    add form to add new item

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 "use strict"
+
+document.getElementById('additem').addEventListener('click', () => {
+  const todoform = document.getElementById('todoform')
+  if (todoform.style.display === 'none') todoform.style.display = 'block'
+  else todoform.style.display = 'none'
+})
