d48d2cbeb7c01bd0d12c9b8021c533b9579283db
Guruh Edi Purwanto <guruhedipurwanto@gmail.com>
2020-01-04 19:28:51 +0800

    add functionality to use local storage and improved style with prettier

--- src/index.js
+++ src/index.js
@@ -1,148 +1,192 @@
-import { getItemFromForm, getProjectTitle } from './get-item-module.js';
-import { populateToDoItem, populateProject } from './populate-ui-module.js';
-import { createProject } from './project-module.js';
-import { createToDoItem } from './to-do-item-module.js';
-import { highlightProject } from './highlight-module';
+"use strict";
 
-"use strict"
+import { getItemFromForm, getProjectTitle } from "./get-item-module.js";
+import { populateToDoItem, populateProject } from "./populate-ui-module.js";
+import { createProject } from "./project-module.js";
+import { createToDoItem } from "./to-do-item-module.js";
+import { highlightProject } from "./highlight-module";
+import { storeToStorage, getFromStorage } from "./local-storage-module.js";
 
 const toDoListController = (function() {
-  var projects = []
+  var projects = [];
+  var currentProject = {};
 
-  var currentProject = createProject('Project 1')
-  projects.push(currentProject)
-  populateProject('Project 1')
+  if (getFromStorage() === null) {
+    currentProject = createProject("Project 1");
+    projects.push(currentProject);
+    populateProject("Project 1");
 
-  var project1 = document.querySelector('.projectlist').lastChild
-  var project1Buttons = project1.querySelectorAll('button')
+    var project1 = document.querySelector(".projectlist").lastChild;
+    var project1Buttons = project1.querySelectorAll("button");
 
-  addFunctionsToProjectButtons(project1Buttons, 0)
+    addFunctionsToProjectButtons(project1Buttons, 0);
+  } else {
+    projects = getFromStorage();
+    document.querySelector(".projectlist").innerHTML = "";
+
+    projects.forEach(project => {
+      populateProject(project.name);
+    });
+
+    const projectList = document.querySelector(".projectlist").childNodes;
+    projectList.forEach((project, index) => {
+      const projectButtons = project.querySelectorAll("button");
+      addFunctionsToProjectButtons(projectButtons, index);
+    });
+
+    currentProject = projects[0];
+    populateProjectToDos(currentProject);
+
+    var toDoList = document.querySelector("tbody").childNodes;
+    toDoList.forEach((toDo, index) => {
+      const toDoButtons = toDo.querySelectorAll("button");
+      addFunctionToToDoButton(toDoButtons, index);
+    });
+  }
 
   function addFunctionToToDoButton(buttonNodeList, index) {
-    buttonNodeList.forEach((button) => {
-      button.setAttribute('data-todo-number', index)
-      if (button.className === 'deletetodo') {
-        button.addEventListener('click', () => {
-          var toDoNumber = event.target.dataset.todoNumber
-          
-          currentProject['items'].splice(toDoNumber, 1)
+    buttonNodeList.forEach(button => {
+      button.setAttribute("data-todo-number", index);
+      if (button.className === "deletetodo") {
+        button.addEventListener("click", () => {
+          var toDoNumber = event.target.dataset.todoNumber;
 
-          populateProjectToDos(currentProject)
+          currentProject["items"].splice(toDoNumber, 1);
 
-          var toDoList = document.querySelector('tbody').childNodes
+          populateProjectToDos(currentProject);
+
+          var toDoList = document.querySelector("tbody").childNodes;
           toDoList.forEach((toDo, index) => {
-            const toDoButtons = toDo.querySelectorAll('button')
-            addFunctionToToDoButton(toDoButtons, index)
-          })
-        })
+            const toDoButtons = toDo.querySelectorAll("button");
+            addFunctionToToDoButton(toDoButtons, index);
+          });
+        });
       }
-    })
+    });
   }
 
   function populateProjectToDos(project) {
-    document.querySelector('tbody').innerHTML = ''
-    const toDos = project['items']    
-    
-    toDos.forEach((toDo) => {
-      populateToDoItem(toDo)
-    })
-  };
+    document.querySelector("tbody").innerHTML = "";
+    const toDos = project["items"];
+
+    toDos.forEach(toDo => {
+      populateToDoItem(toDo);
+    });
+  }
 
   function addFunctionsToProjectButtons(buttonNodeList, index) {
-    buttonNodeList.forEach((button) => {
-      button.setAttribute("data-project-number", index)
+    buttonNodeList.forEach(button => {
+      button.setAttribute("data-project-number", index);
       if (button.className === "viewproject") {
-        button.addEventListener('click', () => {
-          var projectNumber = event.target.dataset.projectNumber
-          var project = projects[projectNumber]
-          currentProject = project
+        button.addEventListener("click", () => {
+          var projectNumber = event.target.dataset.projectNumber;
+          var project = projects[projectNumber];
+          currentProject = project;
 
-          populateProjectToDos(currentProject)
+          populateProjectToDos(currentProject);
 
-          highlightProject()
-        })
-      } else if (button.className === 'deleteproject') {
-        button.addEventListener('click', () => {
-          var projectNumber = event.target.dataset.projectNumber
-          projects.splice(projectNumber, 1)
+          highlightProject();
+        });
+      } else if (button.className === "deleteproject") {
+        button.addEventListener("click", () => {
+          var projectNumber = event.target.dataset.projectNumber;
+          projects.splice(projectNumber, 1);
 
-          document.querySelector('.projectlist').innerHTML = ''
+          document.querySelector(".projectlist").innerHTML = "";
 
-          projects.forEach((project) => {
-            populateProject(project.name)
-          })
-          
-          const projectList = document.querySelector('.projectlist').childNodes
+          projects.forEach(project => {
+            populateProject(project.name);
+          });
+
+          const projectList = document.querySelector(".projectlist").childNodes;
           projectList.forEach((project, index) => {
-            const projectButtons = project.querySelectorAll('button')
-            addFunctionsToProjectButtons(projectButtons, index)
-          })
+            const projectButtons = project.querySelectorAll("button");
+            addFunctionsToProjectButtons(projectButtons, index);
+          });
 
-          currentProject = projects[0]
-          populateProjectToDos(currentProject)
-        })
+          currentProject = projects[0];
+          populateProjectToDos(currentProject);
+        });
       }
-    })    
-  }  
+    });
+  }
 
   function newProject() {
-    var projectTitle = getProjectTitle()
-    const project = createProject(projectTitle)
-    projects.push(project)
-    currentProject = project
-    populateProject(projectTitle)
+    var projectTitle = getProjectTitle();
+    const project = createProject(projectTitle);
+    projects.push(project);
+    currentProject = project;
+    populateProject(projectTitle);
 
-    var lastAddedProject = document.querySelector('.projectlist').lastChild
-    var projectButtons = lastAddedProject.querySelectorAll('button')
-    var index = document.querySelector('.projectlist').childNodes.length - 1
+    var lastAddedProject = document.querySelector(".projectlist").lastChild;
+    var projectButtons = lastAddedProject.querySelectorAll("button");
+    var index = document.querySelector(".projectlist").childNodes.length - 1;
 
-    addFunctionsToProjectButtons(projectButtons, index)
+    addFunctionsToProjectButtons(projectButtons, index);
   }
 
   function newToDo() {
-    var itemDetails = getItemFromForm()
-    const toDo = createToDoItem(itemDetails[0], itemDetails[1], itemDetails[2], itemDetails[3])
-    currentProject.addItem(toDo)
-    populateToDoItem(toDo)
+    var itemDetails = getItemFromForm();
+    const toDo = createToDoItem(
+      itemDetails[0],
+      itemDetails[1],
+      itemDetails[2],
+      itemDetails[3]
+    );
+    currentProject.addItem(toDo);
+    populateToDoItem(toDo);
 
-    var lastAddedToDo = document.querySelector('tbody').lastChild
-    var toDoButtons = lastAddedToDo.querySelectorAll('button')
-    var index = document.querySelector('tbody').childNodes.length - 1
+    var lastAddedToDo = document.querySelector("tbody").lastChild;
+    var toDoButtons = lastAddedToDo.querySelectorAll("button");
+    var index = document.querySelector("tbody").childNodes.length - 1;
 
-    addFunctionToToDoButton(toDoButtons, index)
-  }  
+    addFunctionToToDoButton(toDoButtons, index);
+  }
 
-  return { newProject, newToDo };
+  function storeProjects() {
+    storeToStorage(projects);
+    return null;
+  }
+
+  return { newProject, newToDo, storeProjects };
 })();
 
 (function() {
-  document.getElementById('additem').addEventListener('click', () => {
-    const todoForm = document.getElementById('todoform')
-    if (todoForm.style.display === 'none') todoForm.style.display = 'block'
-    else todoForm.style.display = 'none'
-  })
-  
-  document.getElementById('addproject').addEventListener('click', () => {
-    const projectForm = document.getElementById('projectform')
-    if (projectForm.style.display === 'none') projectForm.style.display = 'block'
-    else projectForm.style.display = 'none'
-  })
+  document.getElementById("additem").addEventListener("click", () => {
+    const todoForm = document.getElementById("todoform");
+    if (todoForm.style.display === "none") todoForm.style.display = "block";
+    else todoForm.style.display = "none";
+  });
 
-  document.getElementById('submititem').addEventListener('click', () => {
-    const todoForm = document.getElementById('todoform')
-    if (todoForm.style.display === 'none') todoForm.style.display = 'block'
-    else todoForm.style.display = 'none'
-  })
-  
-  document.getElementById('submitproject').addEventListener('click', () => {
-    const projectForm = document.getElementById('projectform')
-    if (projectForm.style.display === 'none') projectForm.style.display = 'block'
-    else projectForm.style.display = 'none'
-  })
-  
-  document.getElementById('submitproject').addEventListener(
-    'click', toDoListController.newProject)
-  
-  document.getElementById('submititem').addEventListener(
-    'click', toDoListController.newToDo)
+  document.getElementById("addproject").addEventListener("click", () => {
+    const projectForm = document.getElementById("projectform");
+    if (projectForm.style.display === "none")
+      projectForm.style.display = "block";
+    else projectForm.style.display = "none";
+  });
+
+  document.getElementById("submititem").addEventListener("click", () => {
+    const todoForm = document.getElementById("todoform");
+    if (todoForm.style.display === "none") todoForm.style.display = "block";
+    else todoForm.style.display = "none";
+  });
+
+  document.getElementById("submitproject").addEventListener("click", () => {
+    const projectForm = document.getElementById("projectform");
+    if (projectForm.style.display === "none")
+      projectForm.style.display = "block";
+    else projectForm.style.display = "none";
+  });
+
+  document
+    .getElementById("submitproject")
+    .addEventListener("click", toDoListController.newProject);
+
+  document
+    .getElementById("submititem")
+    .addEventListener("click", toDoListController.newToDo);
+
+  document
+    .getElementById("save")
+    .addEventListener("click", toDoListController.storeProjects);
 })();
---
+++ src/local-storage-module.js
@@ -0,0 +1,9 @@
+function storeToStorage(objectArray) {
+  localStorage.setItem('objects', JSON.stringify(objectArray))
+}
+
+function getFromStorage() {
+  return JSON.parse(localStorage.getItem('objects'));
+}
+
+export { storeToStorage, getFromStorage }
